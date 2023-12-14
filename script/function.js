342c1f1a82fb5c619c630fd04913faf2556c1062
andin <sadboxemailcontato@gmail.com>
2023-12-14 15:42:29 -0300

    adicionado projeto do site

---
+++ script/function.js
@@ -0,0 +1,18 @@
+var prodcomp, mixagem, beatmaker, duvidas, nada;
+
+function prodcomp(){
+    prodcomp = "Produção Completa";
+    document.getElementById('text-area1').innerHTML = prodcomp;
+}
+function mixagem(){
+    mixagem = "Mixagem";
+    document.getElementById('text-area1').innerHTML = mixagem;
+}
+function beatmaker(){
+    beatmaker = "Beatmaker";
+    document.getElementById('text-area1').innerHTML = beatmaker;
+}
+function duvidas(){
+    duvidas = "Dúvidas";
+    document.getElementById('text-area1').innerHTML = duvidas;
+}
